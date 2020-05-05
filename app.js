78da0737debf39695dedf0fddc343f3765796fe3
Paula066 <36409050+Paula066@users.noreply.github.com>
2020-05-05 22:58:42 +0200

    add mixins to media query

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const form = document.querySelector('form');
-const newsletter = document.querySelector('form h1')
-const displayMessage = (e, text) => {
+const newsletter = document.querySelector('.form__thanks')
+const inputForm = document.querySelector('.form__input')
+const displayMessage = (e) => {
     e.preventDefault();
+    newsletter.textContent = "Dziękujemy za zapisanie się do naszego newslettera!";
 
 }
-form.addEventListener('click', displayMessage)
+form.addEventListener('submit', displayMessage)
